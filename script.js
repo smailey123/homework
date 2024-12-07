58a18a90a67fab977520bb1ace07b02e68a16b7c
nikulawx <11022020ua1@gmail.com>
2024-12-07 14:08:54 +0200

    print schedule and add lessons

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-document.cookies =
-  "Math1,8:00,;Math2,9:30,;Math3,11:30,;.History1,8:30,;History2,10:30,;History3,12:30,;.Literature1,8:30,;Literature1,10:30,;Literature1,12:30,;.";
+// document.cookies =
+//   "Math1,8:00,;Math2,9:30,;Math3,11:30,;.History1,8:30,;History2,10:30,;History3,12:30,;.Literature1,8:30,;Literature1,10:30,;Literature1,12:30,;.";
 console.log("Те, як розклад виглядає в кукі", document.cookies);
-
 //перетворення розкладу з кукі у js
 function convertFromCookiesToJs() {
+    let lessons;
+    if(!document.cookies) {
+        console.log('dd')
+        document.cookies = "";
+        lessons = [
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+        ]
+        return lessons;
+    }
   //Ділю роклад за крапкою на дні
   const days = document.cookies.split(".");
   console.log("Дні", days);
@@ -27,7 +41,7 @@ function convertFromCookiesToJs() {
   //   name:"Math",
   //   time:"8:00"
   // }
-  const lessons = lessonsNotFormatted.map((day) =>
+   lessons = lessonsNotFormatted.map((day) =>
     day.map((lesson) => {
       return {
         name: lesson.split(",")[0],
@@ -37,17 +51,73 @@ function convertFromCookiesToJs() {
   );
 
   console.log("Фінальний вигляд розкладу", lessons);
+  return lessons
 }
-convertFromCookiesToJs();
+let lessons = convertFromCookiesToJs();
 let h2 = document.querySelector('h2')
 let dayNumber = 0
 let dayButtons = document.querySelectorAll('.day1')
+let _lessons = document.querySelector('.lessons')
+
+_lessons.innerHTML = ``
+if(lessons[dayNumber]){
+    for(let i = 0;i < lessons[dayNumber].length;i++){
+        _lessons.innerHTML += `<div class="lessons-item1">
+            <p class="lessons-item3">${lessons[dayNumber][i].time} </p>
+            <p class="lessons-item2">${lessons[dayNumber][i].name}</p>
+        </div>`
+    } 
+    
+}
 
 for(let i = 0;i < dayButtons.length;i++){
     dayButtons[i].addEventListener("click", function(){
         dayNumber = i
+        
+        
+        _lessons.innerHTML = ``
+        for(let i = 0;i < lessons[dayNumber].length;i++){
+            _lessons.innerHTML += `<div class="lessons-item1">
+                <p class="lessons-item3">${lessons[dayNumber][i].time} </p>
+                <p class="lessons-item2">${lessons[dayNumber][i].name}</p>
+            </div>`
+        }    
         console.log(dayNumber);
     });
 }
 
+let create_button = document.querySelector('.create-button')
+let nameInput = document.querySelector('.name')
+let time = document.querySelector('.time')
+console.log("sadfdsa",lessons[dayNumber])
+function onAdd(dayNumber, time, name) {
+    lessons[dayNumber].push({ name, time });
+}
 
+function saveToCookies() {
+    let cookiesShedule = "";
+  
+    // document.cookies
+    lessons.forEach((day) => {
+      day.forEach((lesson) => {
+        cookiesShedule += lesson.name + ",";
+        cookiesShedule += lesson.time + ",;";
+      });
+      cookiesShedule += ".";
+    });
+  
+    document.cookies = cookiesShedule;
+    console.log("SADF",cookiesShedule);
+}
+
+create_button.addEventListener("click", function(e){
+    e.preventDefault()
+    onAdd(dayNumber,time.value,nameInput.value)
+    _lessons.innerHTML += `<div class="lessons-item1">
+                <p class="lessons-item3">${lessons[dayNumber][lessons[dayNumber].length - 1].time} </p>
+                <p class="lessons-item2">${lessons[dayNumber][lessons[dayNumber].length - 1].name}</p>
+            </div>`
+    saveToCookies()
+    console.log("les",lessons)
+    console.log('name')
+})
